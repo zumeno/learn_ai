cf150e1f33bd948e0c554f079e136174228c2178
arungeorgesaji <arungeorgesaji@gmail.com>
2025-03-16 20:54:27 +0530

    Fixing final issues

--- ai.py
+++ ai.py
@@ -27,7 +27,6 @@ def initialize_model():
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_compute_dtype=torch.float16, 
-        bnb_4bit_use_double_quant=True,  
         bnb_4bit_quant_type="nf4"  
     )
 
